8d865d49fd3911c8fcdbbbf704ed320b58581230
glax <johanna@bernloehr.eu>
2024-01-15 21:13:26 +0100

    CS2GameState made immutable

--- GameState/CS2GameState.cs
+++ GameState/CS2GameState.cs
@@ -14,4 +14,31 @@ public struct CS2GameState
                $"\t{Map}\n" +
                $"\t{Player}\n";
     }
+
+    internal CS2GameState? UpdateGameStateForLocal(CS2GameState? previousLocalState)
+    {
+        if (previousLocalState is null)
+            return this.Player?.SteamId == ProviderSteamId ? this : null;
+        if (this.Player?.SteamId != ProviderSteamId)
+            return this.WithPlayer(previousLocalState.Value.Player);
+        return this;
+    }
+
+    private CS2GameState WithPlayer(Player? player)
+    {
+        this.Player = player;
+        return this;
+    }
+
+    private CS2GameState WithMap(Map? map)
+    {
+        this.Map = map;
+        return this;
+    }
+
+    private CS2GameState WithTimestamp(int timestamp)
+    {
+        this.Timestamp = timestamp;
+        return this;
+    }
 }
